78f82ecddf93264b1b9940ebf4f214405d11b214
Jin <jaehwanspin@gmail.com>
2022-04-02 15:55:21 +0900

    first

---
+++ c/include/sentient/core_c/reflection.h
@@ -0,0 +1,15 @@
+#ifndef __SENTIENT_CORE_C_REFLECTION_H__
+#define __SENTIENT_CORE_C_REFLECTION_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ c/include/sentient/sentient.h
@@ -0,0 +1,15 @@
+#ifndef __SENTIENT_SENTIENT_H__
+#define __SENTIENT_SENTIENT_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
